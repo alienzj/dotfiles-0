cdc287127b9b81430630068dd799d3bb88c417fa
alienzj <alienchuj@gmail.com>
2022-09-03 22:27:18 +0800

    add home.nix

---
+++ home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "alienzj";
+  home.homeDirectory = "/home/alienzj";
+
+  home.packages = [
+    pkgs.thunderbird
+  ];
+
+  home.stateVersion = "22.05";
+  
+  programs.home-manager.enable = true;
+
+  programs.emacs = {
+    enable = true;
+  };
+ 
+  programs.git = {
+    enable = true;
+    userName = "alienzj";
+    userEmail = "alienchuj@gmail.com";
+  }; 
+
+
+  #xsession.enable = true;
+  #xsession.windowManager.command = "...";
+}
