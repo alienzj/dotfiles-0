da6c0532d284231fadeccc2d95ec23c783626bff
alienzj <alienchuj@gmail.com>
2022-09-04 17:48:26 +0800

    more git

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -3,5 +3,13 @@
     enable = true;
     userName = "alienzj";
     userEmail = "alienchuj@gmail.com";
-  }; 
+  };
+
+  programs.gh = {
+    enable = true;
+  };
+
+  programs.gitui = {
+    enable = true;
+  };
 }
