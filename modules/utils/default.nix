56028729898e50c666a790098ec96792625aee96
alienzj <alienchuj@gmail.com>
2022-09-04 17:47:03 +0800

    more utils

--- modules/utils/default.nix
+++ modules/utils/default.nix
@@ -1,5 +1,46 @@
 {
+  programs.aria2 = {
+    enable = true;
+  };
+
   programs.bat = {
     enable = true;
   };
+
+  programs.less = {
+    enable = true; 
+  };
+
+  programs.lesspipe = {
+    enable = true; 
+  };
+
+  programs.exa = {
+    enable = true;
+  };
+
+  programs.lsd = {
+    enable = true;
+  };
+
+  programs.htop = {
+    enable = true;
+  };
+
+  programs.jq = {
+    enable = true;
+  };
+
+  programs.nnn = {
+    enable = true;
+  };
+
+  programs.pandoc = {
+    enable = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+  };
+ 
 }
