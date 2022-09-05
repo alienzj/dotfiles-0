a9b7c7f9a30b18e4c2824cb32de22a7e8e926cd3
alienzj <alienchuj@gmail.com>
2022-09-06 01:01:15 +0800

    multimedia

--- home/modules/multimedia/default.nix
+++ home/modules/multimedia/default.nix
@@ -3,5 +3,11 @@
   home.packages = with pkgs; [
     vlc
     mpv
+    gimp-with-plugins
+    inkscape-with-extensions
+    blender
+    obs-studio
+    spotify
+    netease-cloud-music-gtk
   ];
 }
