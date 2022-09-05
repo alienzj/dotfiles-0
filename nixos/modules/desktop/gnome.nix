992635d8f58f66a74feee01556aced64121938f0
alienzj <alienchuj@gmail.com>
2022-09-05 13:02:59 +0800

    fix

--- nixos/modules/desktop/gnome.nix
+++ nixos/modules/desktop/gnome.nix
@@ -8,7 +8,7 @@
 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.displayManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   programs.xwayland.enable = true;
 
