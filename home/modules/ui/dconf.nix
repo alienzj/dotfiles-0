145226f6ae012b7f00bf8f786afa1dbffce5a3f1
alienzj <alienchuj@gmail.com>
2022-09-05 21:41:42 +0800

    add dconf

--- home/modules/ui/dconf.nix
+++ home/modules/ui/dconf.nix
@@ -1,9 +1,7 @@
-{ lib, ... }:
-
-with lib.hm.gvariant;
+{ config, lib, pkgs, ... }:
 
 {
-  dconf.settings = {
+  dconf.settings = with lib.hm.gvariant; {
     #"org/gnome/desktop/interface" = {
     #  "icon-theme" = "Adwaita";
     #  "theme" = "Adwaita";
@@ -30,22 +28,22 @@ with lib.hm.gvariant;
 
       "org/gnome/shell/extensions/dash-to-panel" = {
         animate-appicon-hover-animation-extent="{'RIPPLE': 4, 'PLANK': 4, 'SIMPLE': 1}";
-        appicon-margin=4;
-        appicon-padding=4;
-        available-monitors=[0];
-        dot-position="BOTTOM";
-        hotkeys-overlay-combo="TEMPORARILY";
-        intellihide=false;
-        leftbox-padding=-1;
-        panel-anchors="{"0":"MIDDLE"}";
-        panel-element-positions='{"0":[{"element":"showAppsButton","visible":true,"position":"stackedTL"},{"element":"activitiesButton","visible":true,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"centerMonitor"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}';
-        panel-element-positions-monitors-sync=true;
-        panel-lengths="{"0":100}";
-        panel-positions="{"0":"BOTTOM"}";
-        panel-sizes="{"0":30}";
-        status-icon-padding=-1;
-        tray-padding=-1;
-        window-preview-title-position="TOP";
+        appicon-margin = "4";
+        appicon-padding = "4";
+        available-monitors = "[0]";
+        dot-position = "BOTTOM";
+        hotkeys-overlay-combo = "TEMPORARILY";
+        intellihide = false;
+        leftbox-padding = "-1";
+        panel-anchors = "{'0':'MIDDLE'}";
+        panel-element-positions = "{'0':[{'element':'showAppsButton','visible':true,'position':'stackedTL'},{'element':'activitiesButton','visible':true,'position':'stackedTL'},{'element':'leftBox','visible':true,'position':'stackedTL'},{'element':'taskbar','visible':true,'position':'centerMonitor'},{'element':'centerBox','visible':true,'position':'stackedBR'},{'element':'rightBox','visible':true,'position':'stackedBR'},{'element':'dateMenu','visible':true,'position':'stackedBR'},{'element':'systemMenu','visible':true,'position':'stackedBR'},{'element':'desktopButton','visible':true,'position':'stackedBR'}]}";
+        panel-element-positions-monitors-sync = true;
+        panel-lengths = "{'0':100}";
+        panel-positions = "{'0':'BOTTOM'}";
+        panel-sizes = "{'0':30}";
+        status-icon-padding = "-1";
+        tray-padding = "-1";
+        window-preview-title-position = "TOP";
       };
     };
   };
