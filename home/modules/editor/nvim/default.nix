a52bc780c4a1f723c8e280da6d8990c7917b8eab
alienzj <alienchuj@gmail.com>
2022-09-06 01:31:53 +0800

    nvim

--- home/modules/editor/nvim/default.nix
+++ home/modules/editor/nvim/default.nix
@@ -3,12 +3,48 @@
   programs.neovim = {
     enable = true;
 
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    withNodeJs = true;
+    withPython3 = true;
+
     plugins = with pkgs.vimPlugins; [
+      yankring
       bufferline-nvim
       nvim-dap
       nvim-dap-ui
       rust-tools-nvim
       vim-nix
+      { plugin = vim-startify;
+        config = "let g:startify_change_to_vcs_root = 0";
+      }
     ];
+
+    coc = {
+      enable = true;
+      #package = pkgs.vimPlugins.coc-nvim;
+      settings = {
+        "suggest.noselect" = true;
+        "suggest.enablePreview" = true;
+        "suggest.enablePreselect" = false;
+        "suggest.disableKind" = true;
+        languageserver = {
+          haskell = {
+            command = "haskell-language-server-wrapper";
+            args = [ "--lsp" ];
+            rootPatterns = [
+              "*.cabal"
+              "stack.yaml"
+              "cabal.project"
+              "package.yaml"
+              "hie.yaml"
+            ];
+            filetypes = [ "haskell" "lhaskell" ];
+          };
+        };
+      };
+    };
   };
 }
