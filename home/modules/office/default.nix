335f7a67ea529bf83543ce515db1be98e65284be
alienzj <alienchuj@gmail.com>
2022-09-08 11:28:05 +0800

    add syncthing

--- home/modules/office/default.nix
+++ home/modules/office/default.nix
@@ -6,4 +6,31 @@
     thunderbird
     libreoffice
   ];
+
+  services.syncthing = {
+    enable = true;
+    user = "alienzj";
+    group = "users";
+    openDefaultPorts = true;
+
+    dataDir = "/home/alienzj/documents/doraemon";
+    configDir = "/home/alienzj/.config/syncthing";
+
+    devices = {
+      "MAGIC" = { id = "6ZZTZNA-XH7P2CB-FUERPZY-VCYUXCO-PCHPPBF-KG4I7DV-RMZKZJK-YS4GXQU"; };
+    };
+
+    /*
+    folders = {
+      "doraemon" = {
+        path = "/home/alienzj/documents/doraemon";
+        devices = [ "MAGIC" ];
+      };
+      "projects" = {
+        path = "/home/alienzj/projects";
+        devices = [ "MAGIC" ];
+      };
+    };
+    */
+  };
 }
