af0955aadd9aa3e431fe9fd9305127268a5d939f
alienzj <alienchuj@gmail.com>
2022-09-08 11:50:17 +0800

    add syncthing

--- home/modules/office/default.nix
+++ home/modules/office/default.nix
@@ -9,16 +9,18 @@
 
   services.syncthing = {
     enable = true;
-    user = "alienzj";
-    group = "users";
-    openDefaultPorts = true;
+    #user = "alienzj";
+    #group = "users";
+    #openDefaultPorts = true;
 
-    dataDir = "/home/alienzj/documents/doraemon";
-    configDir = "/home/alienzj/.config/syncthing";
+    #dataDir = "/home/alienzj/documents/doraemon";
+    #configDir = "/home/alienzj/.config/syncthing";
 
-    devices = {
-      "MAGIC" = { id = "6ZZTZNA-XH7P2CB-FUERPZY-VCYUXCO-PCHPPBF-KG4I7DV-RMZKZJK-YS4GXQU"; };
-    };
+    tray.enable = true;
+
+    #devices = {
+    #  "MAGIC" = { id = "6ZZTZNA-XH7P2CB-FUERPZY-VCYUXCO-PCHPPBF-KG4I7DV-RMZKZJK-YS4GXQU"; };
+    #};
 
     /*
     folders = {
