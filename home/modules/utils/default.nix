1ae92ad32623a6962a24887c9782e0787c735cbe
alienzj <alienchuj@gmail.com>
2022-09-09 21:17:48 +0800

    add zsh

--- home/modules/utils/default.nix
+++ home/modules/utils/default.nix
@@ -100,5 +100,15 @@
       tmuxPlugins.sessionist
     ];
   };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    ohMyZsh.theme = "lambda";
+    ohMyZsh.enable = true;
+    ohMyZsh.plugins = [ "git" ];
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+  };
  
 }
