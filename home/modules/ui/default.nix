72eed1bcb33393cdc234473a34a389b13e4fac97
alienzj <alienchuj@gmail.com>
2022-09-10 00:44:44 +0800

    save

--- home/modules/ui/default.nix
+++ home/modules/ui/default.nix
@@ -3,6 +3,10 @@
     ./gtk.nix
     ./dconf.nix
     ./font.nix
+
+    ./eww/eww.nix
+
+    ./rofi/rofi.nix
   ];
 
   # reference: https://github.com/nix-community/home-manager/blob/master/modules/services/betterlockscreen.nix
--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -2,6 +2,12 @@
   imports = [
     ./xserver.nix
     ./wayland.nix
-    ./gnome/gnome.nix
+
+    #./gnome/gnome.nix
+
+    ./xmond/xmonad.nix
+    ./picom/picom.nix
+    ./taffybar/taffybar.nix
+    #./polybar/polybar.nix
   ];
 }
