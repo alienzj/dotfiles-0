783159ab97b269f773cec9f3bfcadcc7994a324e
alienzj <alienchuj@gmail.com>
2022-09-11 22:53:51 +0800

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -73,7 +73,7 @@
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.bluetooth.enable = true;
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -81,11 +81,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
