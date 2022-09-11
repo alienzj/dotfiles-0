6488d4e1157687c6f9919d74e30861e44982cc7a
alienzj <alienchuj@gmail.com>
2022-09-11 10:46:59 +0800

    refactor

--- home/modules/desktop/default.nix
+++ home/modules/desktop/default.nix
@@ -1,21 +1,14 @@
 {
   imports = [
-    ./xserver.nix
-    ./wayland.nix
+    #./xserver.nix
+    #./wayland.nix
 
-    #./gnome/gnome.nix
-
-    #./xmond/xmonad.nix
     ./xmond
-
-    #./picom/picom.nix
     ./picom
-
-    #./taffybar/taffybar.nix
     ./taffybar
-
-    #./polybar/polybar.nix
     #./polybar
-
+    ./deadd
+    ./eww
+    ./rofi
   ];
 }
--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -4,18 +4,5 @@
     #./wayland.nix
 
     #./gnome/gnome.nix
-
-    #./xmond/xmonad.nix
-    #./xmond
-
-    #./picom/picom.nix
-    #./picom
-
-    #./taffybar/taffybar.nix
-    #./taffybar
-
-    #./polybar/polybar.nix
-    #./polybar
-
   ];
 }
