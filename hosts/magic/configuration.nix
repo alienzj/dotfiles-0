cfc0a5dbe1795b614b798ddd3b0385b269f162a0
alienzj <alienchuj@gmail.com>
2022-09-12 16:13:03 +0800

    GFLAKE

--- hosts/magic/configuration.nix
+++ hosts/magic/configuration.nix
@@ -100,13 +100,13 @@
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.bluetooth.enable = true;
-  #hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false; 
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    #pulse.enable = true;
+    pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
