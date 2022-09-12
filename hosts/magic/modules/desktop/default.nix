fb5eb27287215e8a2186b24fa48477c1b3885227
alienzj <alienchuj@gmail.com>
2022-09-12 16:04:48 +0800

    magic hardware

---
+++ hosts/magic/modules/desktop/default.nix
@@ -0,0 +1,15 @@
+{
+  imports = [
+    ./xserver.nix
+    #./wayland.nix
+
+    #./gnome
+
+    #./xmonad
+    #./picom
+    #./taffybar
+    #./deadd
+    #./eww
+    #./rofi
+  ];
+}
---
+++ hosts/magic/modules/desktop/xserver.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+{
+  services.blueman.enable = true;
+  services.xserver = {
+    enable = true;
+    dpi = 168;
+    layout = "us";
+    #xkbOptions = "caps:swapescape";
+    xkbVariant = "";
+    videoDrivers = [ "amdgpu" ];
+    #autoRepeatDelay = 200;
+    #autoRepeatInterval = 20;
+
+    #displayManager.setupCommands = ''
+    #  ${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --mode 3840x2160 --scale 0.70x0.70 --output DisplayPort-1 --mode 2560x1440 --rotate left --left-of DisplayPort-0
+    #'';
+
+    libinput = {
+      enable = true;
+
+      # disabling mouse acceleration
+      mouse = {
+        #accelProfile = "flat";
+        accelProfile = "adaptive";
+        accelSpeed = "3.0";
+      };
+
+      # disabling touchpad acceleration
+      touchpad = {
+        accelProfile = "flat";
+        naturalScrolling = true;
+        scrollMethod = "twofinger";
+        tapping = true;
+      };
+
+    };
+
+    desktopManager.session = [
+      {
+        name = "home-manager";
+        start = ''
+          ${pkgs.runtimeShell} $HOME/.hm-xsession &
+          waitPID=$!
+        '';
+      }
+    ];
+  };
+
+  console.font =
+    "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+
+  #environment.variables = {
+  #  GDK_SCALE = "2";
+  #  GDK_DPI_SCALE = "0.5";
+  #  _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  #};
+
+
+  programs.dconf.enable = true;
+}
