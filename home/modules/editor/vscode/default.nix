407d558ece912041c69941d88264e5eab784d9b5
alienzj <alienchuj@gmail.com>
2022-09-13 09:51:49 +0800

    update vscode

--- home/modules/editor/vscode/default.nix
+++ home/modules/editor/vscode/default.nix
@@ -25,6 +25,7 @@
 
       # remote
       ms-vscode-remote.remote-ssh
+      thingalon.pony-ssh
 
       # markdown
       yzhang.markdown-all-in-one
@@ -53,6 +54,13 @@
       golang.go
 
       formulahendry.code-runner
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "remote-ssh-edit";
+        publisher = "ms-vscode-remote";
+        version = "0.47.2";
+        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      }
     ];
 
     userSettings = {
