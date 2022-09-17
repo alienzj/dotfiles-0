8d77379627b7c3ec1625a3e8ed5d78c070769478
alienzj <alienchuj@gmail.com>
2022-09-17 14:28:22 +0800

    save

--- home/modules/desktop/eww/arin/default.nix
+++ home/modules/desktop/eww/arin/default.nix
@@ -1,28 +1,66 @@
 { pkgs, ... }:
 let
-  custom-script-eww-sysinfo = pkgs.writeScriptBin "custom-script-eww-sysinfo" ''
+  custom-script-eww-battery = pkgs.writeScriptBin "custom-script-eww-battery" ''
     #!/${pkgs.stdenv.shell}
-    ${builtins.readFile ./scripts/custom-eww-sysinfo.sh}
+    ${builtins.readFile ./scripts/battery}
+  '';
+  custom-script-eww-check-network = pkgs.writeScriptBin "custom-script-eww-check-network" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/check-network}
+  '';
+  custom-script-eww-mails = pkgs.writeScriptBin "custom-script-eww-mails" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/mails}
+  '';
+  custom-script-eww-music_info = pkgs.writeScriptBin "custom-script-eww-music_info" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/music_info}
+  '';
+  custom-script-eww-quotes = pkgs.writeScriptBin "custom-script-eww-quotes" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/quotes}
+  '';
+  custom-script-eww-sys_info = pkgs.writeScriptBin "custom-script-eww-sys_info" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/sys_info}
+  '';
+  custom-script-eww-system = pkgs.writeScriptBin "custom-script-eww-system" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/system}
+  '';
+  custom-script-eww-volume = pkgs.writeScriptBin "custom-script-eww-volume" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/volume}
+  '';
+  custom-script-eww-weather_info = pkgs.writeScriptBin "custom-script-eww-weather_info" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/weather_info}
+  '';
+  custom-script-eww-widget_apps = pkgs.writeScriptBin "custom-script-eww-widget_apps" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/widget_apps}
+  '';
+  custom-script-eww-widget_search = pkgs.writeScriptBin "custom-script-eww-widget_search" ''
+    #!/${pkgs.stdenv.shell}
+    ${builtins.readFile ./scripts/widget_search}
   '';
 in
 {
   home.packages = with pkgs; [
-    custom-script-eww-sysinfo
+    custom-script-eww-battery
+    custom-script-eww-check-network
+    custom-script-eww-mails
+    custom-script-eww-music_info
+    custom-script-eww-quotes
+    custom-script-eww-sys_info
+    custom-script-eww-system
+    custom-script-eww-volume
+    custom-script-eww-weather_info
+    custom-script-eww-widget_apps
+    custom-script-eww-widget_search
     eww
   ];
 
   home.file.".config/eww/eww.scss".source = ./eww.scss;
   home.file.".config/eww/eww.yuck".source = ./eww.yuck;
-
-  # So eww daemon starts in the parent shell, It executes the scripts
-  # provided in that shell environment only. That environment does not have
-  # access to the session variables that I declare in zsh.
-  # TODO: Figure out the exact sequence of evnets that happen here
-  # To provide the required session variables to eww daemon, importing the
-  # variables here.
-
-  #xsession.profileExtra = ''
-  #  export DOTFILES=~/.dotfiles
-  #  ${builtins.readFile ../../../secrets/env-vars.sh}
-  #'';
 }
