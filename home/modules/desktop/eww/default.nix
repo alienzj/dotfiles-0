d5020097d059fc65aac980bfd3e24a7500db5764
alienzj <alienchuj@gmail.com>
2022-09-17 13:54:26 +0800

    save

--- home/modules/desktop/eww/default.nix
+++ home/modules/desktop/eww/default.nix
@@ -1,28 +1,11 @@
-{ pkgs, ... }:
-let
-  custom-script-eww-sysinfo = pkgs.writeScriptBin "custom-script-eww-sysinfo" ''
-    #!/${pkgs.stdenv.shell}
-    ${builtins.readFile ./scripts/custom-eww-sysinfo.sh}
-  '';
-in
+{ config, pkgs, ... }:
+
 {
-  home.packages = with pkgs; [
-    custom-script-eww-sysinfo
-    eww
+  imports = [
+    #./arin
+    #./dashboard
+    ./dashboard_
+    #./eww-bar
   ];
 
-  home.file.".config/eww/eww.scss".source = ./eww.scss;
-  home.file.".config/eww/eww.yuck".source = ./eww.yuck;
-
-  # So eww daemon starts in the parent shell, It executes the scripts
-  # provided in that shell environment only. That environment does not have
-  # access to the session variables that I declare in zsh.
-  # TODO: Figure out the exact sequence of evnets that happen here
-  # To provide the required session variables to eww daemon, importing the
-  # variables here.
-
-  #xsession.profileExtra = ''
-  #  export DOTFILES=~/.dotfiles
-  #  ${builtins.readFile ../../../secrets/env-vars.sh}
-  #'';
 }
