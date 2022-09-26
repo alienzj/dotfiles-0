9bd126961f756a084653e4f979b9fe5dc1d28748
alienzj <alienchuj@gmail.com>
2022-09-27 05:43:14 +0800

    save

--- hosts/eniac/modules/desktop/xserver.nix
+++ hosts/eniac/modules/desktop/xserver.nix
@@ -15,9 +15,9 @@
     #displayManager.setupCommands = ''
     #  ${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --mode 3840x2160 --scale 0.70x0.70 --output DisplayPort-1 --mode 2560x1440 --rotate left --left-of DisplayPort-0
     #'';
-    displayManager.setupCommands = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --mode 3840x2160 --scale 0.70x0.70 --output DisplayPort-1 --mode 2560x1440 --rotate left --left-of DisplayPort-0
-    '';
+    #displayManager.setupCommands = ''
+    #  ${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --mode 3840x2160 --scale 0.70x0.70 --output DisplayPort-1 --mode 2560x1440 --rotate left --left-of DisplayPort-0
+    #'';
 
 
     libinput = {
