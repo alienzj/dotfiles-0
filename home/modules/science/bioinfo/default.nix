62115cfda0a2780012945eea3ccf5e96c9317155
alienzj <alienchuj@gmail.com>
2022-10-30 18:03:25 +0800

    update bioinfo

--- home/modules/science/bioinfo/default.nix
+++ home/modules/science/bioinfo/default.nix
@@ -2,11 +2,37 @@
 
 {
   home.packages = with pkgs; [
-    #cutadapt
-    megahit
-    #spades
-    bwa
+    bcftools
+    bedtools
+    blast
     bowtie2
+    bwa
+    cd-hit
+    clustal-omega
+    deeptools
+    diamond
+    eggnog-mapper
+    fastp
+    freebayes
+    gatk
+    hisat2
+    hmmer
     igv
+    iqtree
+    kallisto
+    mafft
+    megahit
+    minimap2
+    muscle
+    picard-tools
+    plink-ng
+    prodigal
+    quast
+    raxml
+    raxml-mpi
+    sambamba
+    samtools
+    star
+    vcftools
   ];
 }
